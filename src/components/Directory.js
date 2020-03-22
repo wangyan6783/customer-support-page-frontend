640e6127ed7c2acbadb3a50e6be3b3559fe01ca4
J.SIMPLE <wangyan6783@gmail.com>
2020-03-22 05:00:55 -0500

    update href link

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -4,7 +4,7 @@ import directoryContent from "../data/directory-content";
 function Directory() {
   return (
     <div className="directory">
-      <a href="/" className="main-link-text">
+      <a href="/customer-support-page-frontend/" className="main-link-text">
         {directoryContent.mainDirectory}
       </a>
       <p className="sub-text inline">{` / ${directoryContent.subDirectory}`}</p>
