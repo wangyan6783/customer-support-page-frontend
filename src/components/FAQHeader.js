de2ec698b082cbf5e225b34e872fadcd76f31ab3
J.SIMPLE <wangyan6783@gmail.com>
2020-03-22 04:53:01 -0500

    update href

--- src/components/FAQHeader.js
+++ src/components/FAQHeader.js
@@ -9,7 +9,7 @@ function FAQHeader() {
         <h1>{faqHeaderContent.title1}</h1>
         <h1>{faqHeaderContent.title2}</h1>
       </div>
-      <a href="#" className="main-link-text">
+      <a href="/" className="main-link-text">
         {faqHeaderContent.link}
       </a>
     </header>
