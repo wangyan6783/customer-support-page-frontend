8cfb67fb8ecda08efe5319e4d602a1395ff774a9
J.SIMPLE <wangyan6783@gmail.com>
2020-03-22 04:55:00 -0500

    Updates

---
+++ precache-manifest.6ed6b22e6254655023e815c51adc1d7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65526cbaef1cde50296fe1f076f71782",
+    "url": "/customer-support-page-frontend/index.html"
+  },
+  {
+    "revision": "eb65ea1f1c5d7ef92af1",
+    "url": "/customer-support-page-frontend/static/css/main.0fcc07fd.chunk.css"
+  },
+  {
+    "revision": "81130226d2e561514f2e",
+    "url": "/customer-support-page-frontend/static/js/2.7142bf80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/customer-support-page-frontend/static/js/2.7142bf80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb65ea1f1c5d7ef92af1",
+    "url": "/customer-support-page-frontend/static/js/main.31e3e3e9.chunk.js"
+  },
+  {
+    "revision": "dbef19233c498a201bff",
+    "url": "/customer-support-page-frontend/static/js/runtime-main.f1f0ff11.js"
+  }
+]);
